9cd9104c4615019164d8bdb12d5d0d89e47bea7f
Alex B <abailon949@gmail.com>
2020-03-09 11:37:31 -0500

    Added comments to js. Added img for readme and finished readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # day-planner
  
  ## Summary
- With this day planner, the user the has the ablity to have a daily planner and save events to local storage. The uses moment js to get the current date and time. With the use of moment js, the code is able to know if its a new day for the user so that it may reset the events in local storage but if the user comes back the same day all events are still saved in local storage. If the current time is past the event time the event will shade out. If the event is currently going on the event turns red and if the event is in the future  the event will be green.
+ With this day planner, the user the has the ability to have a daily planner and save events to local storage. This uses moment js to get the current date and time. With the use of moment js, the code is able to know if it's a new day for the user so that it may reset the events in local storage but if the user comes back the same day all events are still saved in local storage. If the current time is past the event time the event will shade out. If the event is currently going on the event turns red and if the event is in the future the event will be green.
+
+ ## Deployed Webpage
+
+Link to webpage: [day-planner](https://alex-bailon.github.io/day-planner/)
+
+Below are screenshots of webpage: ![homepage to day planner](./assets/day-planner-exp.png)
+
+## Summary
+With the use of moment js and jquery, I was able to make a day planner that would create the html for the time blocks automatically. Future goals for this project would be to give the user the ability to select a date.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-$('#currentDay').text(moment().format("dddd, MMMM Do"));
-let today = moment().format('dddd, MMMM Do')
-let todayStored = moment().format('dddd, MMMM Do')
-initDate()
+let today = moment().format('dddd, MMMM Do') //sets todays date into a var
+$('#currentDay').text(today); //display todays date on webpage
+let todayStored = moment().format('dddd, MMMM Do') //stores todays date into a different var to be able to check if its a new date
+initDate() //if there is a date in local storage will replace todays date with whatever is in local storage
+//var for all business hours with no events.
 let hoursArray = [
     {
         hour: '9AM',
@@ -43,20 +44,21 @@ let hoursArray = [
 ]
 
 let hours = hoursArray
-
+// checks to see if today and todayStored are the same date, if so it will check if there is anything in local storage for hours array
 if (today == todayStored){
     init()
     localStorage.setItem('todayStored', JSON.stringify(todayStored))   
-    console.log(hours)
 }
+// else means that its a new date so it will reset the hours array to start a new day.
 else{
     hours = hoursArray
-    console.log(hours)
     todayStored = today
     localStorage.setItem('todayStored', JSON.stringify(todayStored))
+    localStorage.setItem('hours', JSON.stringify(hours))
 }
-let h = 9
 
+let h = 9 //var to help name rowDiv class. Should be set to whatever the first our of hours array is.
+// creats a row for all the hours within array
 for (var i = 0; i < hours.length; i++){
     let rowDiv = $('<div>')
     let hourCol = $('<div>')
@@ -71,7 +73,7 @@ for (var i = 0; i < hours.length; i++){
     textArea.addClass('col-md-10 timeEvent')
     textArea.text(hours[i].timeEvent)
     saveButton.addClass('btn saveBtn col-md-1')
-    saveIcon.addClass('fas fa-save')
+    saveIcon.addClass('fa fa-save')
     saveIcon.appendTo(saveButton)
     hourCol.appendTo(rowDiv)
     textArea.appendTo(rowDiv)
@@ -79,46 +81,47 @@ for (var i = 0; i < hours.length; i++){
     $('.container').append(rowDiv)
 }
 
+//function to check current hour for all the rows it will check what hour id the row has and compare it the the current hour
 function currentHour(){
     let rightNow = moment().hours()
-    console.log(rightNow)
-    $(".time-block").each(function() {
-        var blockHour = +($(this).attr("id").split("-")[1])
-        console.log(blockHour)
-  
-        if (blockHour < rightNow) {
-          $(this).addClass("past")
+    $('.time-block').each(function() {
+        let hourBlock = +($(this).attr('id').split('-')[1])  
+        //if row hour is less than the current hour it will add class past which changes the color of the row to gray
+        if (hourBlock < rightNow) {
+          $(this).addClass('past')
         } 
-        else if (blockHour == rightNow) {
-          $(this).removeClass("past")
-          $(this).addClass("present")
+        //if the row hour is the same it will give the row the present class which will change the color to red
+        else if (hourBlock == rightNow) {
+          $(this).removeClass('past')
+          $(this).addClass('present')
         } 
+        //else it means its a future event which will change the color to green
         else {
-          $(this).removeClass("past")
-          $(this).removeClass("present")
-          $(this).addClass("future")
+          $(this).removeClass('past')
+          $(this).removeClass('present')
+          $(this).addClass('future')
         }
       });
 }
 
 currentHour()
 
-var updateHour = setInterval(currentHour, 30000);
-
+let updateHour = setInterval(currentHour, 30000); //var to run the currentHour function every 30 seconds
+//on click event for save buttons. Once clicked it will get save the event for that hour block into local storage
 $('.saveBtn').on('click', function(){
     let val = $(this).siblings('.timeEvent').val()
     let time = +($(this).parent().attr('id').split(' ')[0])
     hours[time].timeEvent = val
     localStorage.setItem('hours', JSON.stringify(hours))
 })
-
+//funcation to retrive hours array from local storage
 function init(){
     var retrive = JSON.parse(localStorage.getItem('hours'))
     if (retrive !== null){
         hours = retrive
     }
 }
-
+//funcation to retrive the stored date from local storage
 function initDate(){
     var ret = JSON.parse(localStorage.getItem('todayStored'))
     if (ret !== null){
